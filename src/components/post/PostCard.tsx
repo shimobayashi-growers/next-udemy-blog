4785bf1a7a333fcfd7e066cba3a55c974b6df717
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-21 19:39:11 +0900

    feat: posts/[id]ページの作成

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function PostCard({ post }: PostCardProps) {
     return (
         <Card className="hover:shadow-md transition-shadow">
-            <Link href="{`/posts/${post.id}`}">
+            <Link href={`/posts/${post.id}`}>
                 {post.topImage && (
                     <div className="relative w-full h-48">
                         <Image
--- src/lib/post.ts
+++ src/lib/post.ts
@@ -18,3 +18,18 @@ export async function getPosts() {
     });
     return posts;
 }
+
+export async function getPost(id: string) {
+    return await prisma.post.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            author: {
+                select: {
+                    name: true,
+                },
+            },
+        },
+    });
+}
