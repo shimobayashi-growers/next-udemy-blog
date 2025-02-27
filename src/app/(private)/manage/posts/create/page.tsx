b1d1dcc6099520743efb54e8b33d94b2aa49de4f
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-27 16:57:55 +0900

    feat: 記事作成機能の完成

--- src/app/(private)/manage/posts/create/page.tsx
+++ src/app/(private)/manage/posts/create/page.tsx
@@ -9,12 +9,18 @@ import "highlight.js/styles/github-dark.css";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { createPost } from "@/lib/actions/createPost";
 
 export default function CreatePage() {
 
     const [content, setContent] = useState("");
     const [contentLength, setContentLength] = useState(0);
     const [preview, setPreview] = useState(false);
+
+    const [state, formAction] = useActionState(createPost, {
+        success: false,
+        errors: {},
+    });
 
     const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const value = e.target.value;
@@ -25,10 +31,20 @@ export default function CreatePage() {
     return (
         <div className="container mx-auto mt-10">
             <h1 className="text-2xl font-bold mb-4">新規投稿</h1>
-            <form className="space-y-4">
+            <form action={formAction} className="space-y-4">
                 <div>
                     <Label htmlFor="title">タイトル</Label>
-                    <Input id="title" type="text" placeholder="タイトルを入力してください" />
+                    <Input 
+                        id="title" 
+                        name="title"
+                        type="text" 
+                        placeholder="タイトルを入力してください" 
+                    />
+                    {state.errors.title && (
+                        <p className="text-red-500 text-sm mt-1">
+                            {state.errors.title.join(", ")}
+                        </p>
+                    )}
                 </div>
                 <div>
                     <Label htmlFor="topImage">画像</Label>
@@ -38,6 +54,11 @@ export default function CreatePage() {
                         accept="image/*"
                         name="topImage"
                     />
+                    {state.errors.topImage && (
+                        <p className="text-red-500 text-sm mt-1">
+                            {state.errors.topImage.join(", ")}
+                        </p>
+                    )}
                 </div>
                 <div>
                     <Label htmlFor="content">内容</Label>
@@ -51,6 +72,11 @@ export default function CreatePage() {
                         onChange={handleContentChange}
                     />
                 </div>
+                {state.errors.content && (
+                    <p className="text-red-500 text-sm mt-1">
+                        {state.errors.content.join(", ")}
+                    </p>
+                )}
                 <div className="text-right text-sm text-gray-500 mt-1">
                     文字数： {contentLength}
                 </div>
@@ -72,11 +98,11 @@ export default function CreatePage() {
                             unwrapDisallowed={true} // Markdownの改行を許可
                         >{content}
                         </ReactMarkdown>
-                        <Button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                            投稿する
-                        </Button>
                     </div>
                 )}
+                <Button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                    投稿する
+                </Button>
             </form>
         </div>
     )
