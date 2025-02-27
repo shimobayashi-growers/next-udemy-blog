ea094f3b807d9dcdca57987bce83687ed48980d1
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-27 15:24:53 +0900

    fix: createUser

--- src/lib/actions/createUser.ts
+++ src/lib/actions/createUser.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { redirect } from "next/navigation";
 import { signIn } from "@/auth";
+import { ZodError } from "zod";
 
 type ActionState = {
     success: boolean;
@@ -12,9 +13,10 @@ type ActionState = {
 }
 
 // バリデーションエラーを処理する関数
-function handleValidationError(error: any): ActionState{
+function handleValidationError(error: ZodError): ActionState{
     const { fieldErrors, formErrors } = error.flatten();
-    // zodの私用でC0mpl3x!P@ssw0rd一致確認のエラーはformErrorsでわたってくる
+    const castedFieldErrors = fieldErrors as Record<string, string[]>;
+    // zodの使用でパスワード一致確認のエラーはformErrorsでわたってくる
     // それ以外のエラーはfieldErrorsでわたってくる
     if(formErrors.length > 0){
         return {
@@ -27,7 +29,7 @@ function handleValidationError(error: any): ActionState{
     }
     return {
         success: false,
-        errors: fieldErrors,
+        errors: castedFieldErrors,
     };
 }
 
