d487120da9edef93c1f023d6d49b65b123b54001
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-28 14:15:23 +0900

    add: editページの新規作成

---
+++ src/app/(private)/manage/posts/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@/auth";
+import { getOwnPost } from "@/lib/ownPost";
+import { notFound } from "next/navigation";
+import EditPostForm from "./EditPostForm";
+type Params = {
+    params: Promise<{ id: string }>;
+};
+
+export default async function EditPage({ params }: Params){
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if(!session?.user?.email || !userId){
+        throw new Error("ユーザー情報がありません");
+    }
+
+    const { id } = await params;
+    const post = await getOwnPost(userId, id);
+
+    // 記事が存在しない場合は404を返す
+    if (!post) {
+        notFound();
+    }
+
+    return (
+        <></>
+        // <EditPostForm post={post} />
+    )
+}
