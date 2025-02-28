bc4b5836a0c4877a0023830c7163ffc79b0d507c
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-28 11:56:30 +0900

    add: manage/posts/[id]の表示

---
+++ src/app/(private)/manage/posts/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Post not found</h1>
+            <p>お探しの記事は存在しないか、削除されています。</p>
+        </div>
+    );
+}
