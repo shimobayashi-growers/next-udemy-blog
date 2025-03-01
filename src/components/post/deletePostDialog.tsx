cad894d6058899e0aa2e3c8d8754565a0b59191f
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-03-01 11:48:26 +0900

    add: 記事削除のaleatdialogの作成

---
+++ src/components/post/deletePostDialog.tsx
@@ -0,0 +1,40 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+  } from "@/components/ui/alert-dialog"
+
+type DeletePostProps = {
+    postId: string;
+    isOpen: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export default function DeletePostDialog({ postId, isOpen, onOpenChange }: DeletePostProps) {
+    return (
+        <AlertDialog open={isOpen} onOpenChange={onOpenChange}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>記事の削除</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        この記事を削除してよろしいですか？
+                        <br />
+                        この操作は元に戻すことはできません。
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>キャンセル</AlertDialogCancel>
+                    <AlertDialogAction  className="bg-red-500 hover:bg-red-600">
+                        削除する
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+
+    )
+}
